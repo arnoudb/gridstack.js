4b00e727e88ee808e9ad654c4984bf334567b259
Arnoud Bos <arnoud@crunchrapps.com>
2023-11-14 16:50:00 +0100

    update demo

--- angular/projects/demo/src/app/app.component.ts
+++ angular/projects/demo/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { GridStack, GridStackOptions, GridStackWidget } from 'gridstack';
 import { AngularSimpleComponent } from './simple';
 import { AngularNgForTestComponent } from './ngFor';
@@ -6,18 +6,17 @@ import { AngularNgForCmdTestComponent } from './ngFor_cmd';
 
 // NOTE: local testing of file
 // import { GridstackComponent, NgGridStackOptions, NgGridStackWidget, elementCB, gsCreateNgComponents, nodesCB } from './gridstack.component';
-import {
-  GridstackComponent, NgGridStackOptions, NgGridStackWidget, elementCB, gsCreateNgComponents, nodesCB,droppedCB
-} from 'gridstack/dist/angular';
+import { GridstackComponent, NgGridStackOptions, NgGridStackWidget, elementCB, gsCreateNgComponents, nodesCB, droppedCB } from 'gridstack/dist/angular';
 
 // unique ids sets for each item for correct ngFor updating
 let ids = 1;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
 
   @ViewChild(AngularSimpleComponent) case0Comp?: AngularSimpleComponent;
   @ViewChild(AngularNgForTestComponent) case1Comp?: AngularNgForTestComponent;
@@ -40,7 +39,7 @@ export class AppComponent implements OnInit {
     // float: true,
     minRow: 1,
   }
-  private sub0: NgGridStackWidget[] = [{x:0, y:0, selector:'app-a'}, {x:1, y:0, content:'plain html'}, {x:0, y:1, selector:'app-b'} ];
+  private sub0: NgGridStackWidget[] = [{x: 0, y: 0, selector: 'app-a'}, {x: 1, y: 0, content: 'plain html'}, {x: 0, y: 1, selector: 'app-b'}];
   public gridOptionsFull: NgGridStackOptions = {
     ...this.gridOptions,
     children: this.sub0,
@@ -53,12 +52,12 @@ export class AppComponent implements OnInit {
     acceptWidgets: true, // will accept .grid-stack-item by default
     margin: 5,
   };
-  private sub1: NgGridStackWidget[] = [ {x:0, y:0, selector:'app-a'}, {x:1, y:0, selector:'app-b'}, {x:2, y:0, selector:'app-c'}, {x:3, y:0}, {x:0, y:1}, {x:1, y:1}];
-  private sub2: NgGridStackWidget[] = [ {x:0, y:0}, {x:0, y:1, w:2}];
+  private sub1: NgGridStackWidget[] = [{x: 0, y: 0, selector: 'app-a'}, {x: 1, y: 0, selector: 'app-b'}, {x: 2, y: 0, selector: 'app-c'}, {x: 3, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}];
+  private sub2: NgGridStackWidget[] = [{x: 0, y: 0}, {x: 0, y: 1, w: 2}];
   private subChildren: NgGridStackWidget[] = [
-    {x:0, y:0, content: 'regular item'},
-    {x:1, y:0, w:4, h:4, subGridOpts: {children: this.sub1, class: 'sub1', ...this.subOptions}},
-    {x:5, y:0, w:3, h:4, subGridOpts: {children: this.sub2, class: 'sub2', ...this.subOptions}},
+    {x: 0, y: 0, content: 'regular item'},
+    {x: 1, y: 0, w: 4, h: 4, subGridOpts: {children: this.sub1, class: 'sub1', ...this.subOptions}},
+    {x: 5, y: 0, w: 3, h: 4, subGridOpts: {children: this.sub2, class: 'sub2', ...this.subOptions}},
   ]
   public nestedGridOptions: NgGridStackOptions = { // main grid options
     cellHeight: 50,
@@ -84,7 +83,7 @@ export class AppComponent implements OnInit {
       {x: 2, y: 3, w: 3, maxW: 3, id: 'special', content: 'has maxW=3'},
     ]
   };
-  public twoGridOpt2: NgGridStackOptions = { ...this.twoGridOpt1, float: false }
+  public twoGridOpt2: NgGridStackOptions = {...this.twoGridOpt1, float: false}
   private serializedData?: NgGridStackOptions;
 
   constructor() {
@@ -121,17 +120,33 @@ export class AppComponent implements OnInit {
     // let the switch take affect then load the starting values (since we sometimes save())
     setTimeout(() => {
       let data;
-      switch(val) {
-        case 0: data = this.case0Comp?.items; break;
-        case 1: data = this.case1Comp?.items; break;
-        case 2: data = this.case2Comp?.items; break;
-        case 3: data = this.gridComp?.grid?.save(true, true); break;
-        case 4: data = this.items; break;
-        case 5: data = this.gridOptionsFull; break;
-        case 6: data = this.nestedGridOptions; break;
-        case 7: data = this.twoGridOpt1;
-          GridStack.setupDragIn('.sidebar .grid-stack-item', { appendTo: 'body', helper: 'clone' });
-          break;
+      switch (val) {
+      case 0:
+        data = this.case0Comp?.items;
+        break;
+      case 1:
+        data = this.case1Comp?.items;
+        break;
+      case 2:
+        data = this.case2Comp?.items;
+        break;
+      case 3:
+        data = this.gridComp?.grid?.save(true, true);
+        break;
+      case 4:
+        data = this.items;
+        break;
+      case 5:
+        data = this.gridOptionsFull;
+        break;
+      case 6:
+        data = this.nestedGridOptions;
+        break;
+      case 7:
+      case 8:
+        data = this.twoGridOpt1;
+        GridStack.setupDragIn('.sidebar .grid-stack-item', {appendTo: 'body', helper: 'clone'});
+        break;
       }
       if (this.origTextEl) this.origTextEl.nativeElement.value = JSON.stringify(data, null, '  ');
     });
@@ -155,8 +170,9 @@ export class AppComponent implements OnInit {
   public add() {
     // TODO: BUG the content doesn't appear until widget is moved around (or another created). Need to force
     // angular detection changes...
-    this.gridComp?.grid?.addWidget({x:3, y:0, w:2, content:`item ${ids}`, id:String(ids++)});
+    this.gridComp?.grid?.addWidget({x: 3, y: 0, w: 2, content: `item ${ids}`, id: String(ids++)});
   }
+
   public delete() {
     let grid = this.gridComp?.grid;
     if (!grid) return;
@@ -168,15 +184,17 @@ export class AppComponent implements OnInit {
     }
     if (node) grid.removeWidget(node.el!);
   }
+
   public modify() {
-    this.gridComp?.grid?.update(this.gridComp?.grid.engine.nodes[0]?.el!, {w:3})
+    this.gridComp?.grid?.update(this.gridComp?.grid.engine.nodes[0]?.el!, {w: 3})
   }
+
   public newLayout() {
     this.gridComp?.grid?.load([
-      {x:0, y:1, id:'1', minW:1, w:1}, // new size/constrain
-      {x:1, y:1, id:'2'},
+      {x: 0, y: 1, id: '1', minW: 1, w: 1}, // new size/constrain
+      {x: 1, y: 1, id: '2'},
       // {x:2, y:1, id:'3'}, // delete item
-      {x:3, y:0, w:2, content:'new item'}, // new item
+      {x: 3, y: 0, w: 2, content: 'new item'}, // new item
     ]);
   }
 
@@ -186,33 +204,39 @@ export class AppComponent implements OnInit {
   public addNgFor() {
     // new array isn't required as Angular detects changes to content with trackBy:identify()
     // this.items = [...this.items, { x:3, y:0, w:3, content:`item ${ids}`, id:String(ids++) }];
-    this.items.push({x:3, y:0, w:2, content:`item ${ids}`, id:String(ids++)});
+    this.items.push({x: 3, y: 0, w: 2, content: `item ${ids}`, id: String(ids++)});
   }
+
   public deleteNgFor() {
     this.items.pop();
   }
+
   public modifyNgFor() {
     // this will not update the DOM nor trigger gridstackItems.changes for GS to auto-update, so set new option of the gridItem instead
     // this.items[0].w = 3;
     const gridItem = this.gridComp?.gridstackItems?.get(0);
-    if (gridItem) gridItem.options = {w:3};
+    if (gridItem) gridItem.options = {w: 3};
   }
+
   public newLayoutNgFor() {
     this.items = [
-      {x:0, y:1, id:'1', minW:1, w:1}, // new size/constrain
-      {x:1, y:1, id:'2'},
+      {x: 0, y: 1, id: '1', minW: 1, w: 1}, // new size/constrain
+      {x: 1, y: 1, id: '2'},
       // {x:2, y:1, id:'3'}, // delete item
-      {x:3, y:0, w:2, content:'new item'}, // new item
+      {x: 3, y: 0, w: 2, content: 'new item'}, // new item
     ];
   }
+
   public clearGrid() {
     if (!this.gridComp) return;
     this.gridComp.grid?.removeAll(true);
   }
+
   public saveGrid() {
     this.serializedData = this.gridComp?.grid?.save(false, true) as GridStackOptions || ''; // no content, full options
     if (this.textEl) this.textEl.nativeElement.value = JSON.stringify(this.serializedData, null, '  ');
   }
+
   public loadGrid() {
     if (!this.gridComp) return;
     GridStack.addGrid(this.gridComp.el, this.serializedData);
@@ -223,8 +247,39 @@ export class AppComponent implements OnInit {
     return w.id; // or use index if no id is set and you only modify at the end...
   }
 
+  ///////////////////////////////
+  //
+  // demo8 code
+  //
+  ///////////////////////////////
+  @ViewChild('gridStack2') gridStack2?: GridstackComponent;
 
-  // demo 8 code
+  @ViewChild('gridStack1') gridStack1?: GridstackComponent;
+
+  ngAfterViewInit(): void {
+    // monkeypatch grid 1
+    this.gridStack1?.grid?.on('dropped', (event, previousNode, newNode) => {
+      console.log('remove the node');
+      this.gridStack1?.grid?.engine.removeNode(newNode);
+      //setTimeout(() => {
+        console.log('dropped on grid1 is delayed ');
+        this.gridStack1?.droppedCB.emit({ event, previousNode, newNode });
+      //}, 10);
+    });
+
+    // monkeypatch grid 2
+    this.gridStack2?.grid?.on('dropped', (event, previousNode, newNode) => {
+      console.log('remove the node');
+      this.gridStack2?.grid?.engine.removeNode(newNode);
+      //setTimeout(() => {
+        console.log('dropped on grid2 is delayed ');
+        this.gridStack2?.droppedCB.emit({ event, previousNode, newNode });
+      //}, 10);
+    });
+  }
+
+  private draggedItem: GridStackWidget | null = null;
+
   protected demo8GridOpt1: NgGridStackOptions = {
     column: 6,
     cellHeight: 50,
@@ -234,19 +289,60 @@ export class AppComponent implements OnInit {
     acceptWidgets: true,
   };
 
-  protected demo8GridOpt2: NgGridStackOptions = { ...this.demo8GridOpt1 };
+  protected demo8GridOpt2: NgGridStackOptions = {...this.demo8GridOpt1};
 
-  protected demo8GridElements1:GridStackWidget[] = [
-    {id:'id_1', x: 0, y: 0, w: 2, h: 2, content: '1'},
-    {id:'id_2', x: 3, y: 1, h: 2, content: '2'},
-    {id:'id_3', x: 4, y: 1, content: '3' },
-    {id:'id_4', x: 2, y: 3, w: 3, maxW: 3, content: '4: has maxW=3'}
+  protected demo8GridElements1: GridStackWidget[] = [
+    {id: 'id_1', x: 0, y: 0, w: 2, h: 2, content: '1'},
+    {id: 'id_2', x: 3, y: 1, h: 2, content: '2'},
+    {id: 'id_3', x: 4, y: 1, content: '3'},
+    {id: 'id_4', x: 2, y: 3, w: 3, maxW: 3, content: '4: has maxW=3'}
   ];
 
-  protected demo8GridElements2:GridStackWidget[] = [ { id:'id_5', x: 4, y: 1, content: '5' },];
+  protected demo8GridElements2: GridStackWidget[] = [
+    {id: 'id_5', x: 4, y: 1, content: '5'},
+  ];
 
-  // drop item from left to right grid
-  protected demo8DroppedCB(event: droppedCB):void {
-    this.demo8GridElements1 = [];
+  protected demo8DragStart(event: elementCB, grid: "1" | "2") {
+    console.log('dragstart', event);
+
+    if (grid === '1') {
+      this.draggedItem = this.demo8GridElements1.find(el => el.id === event.el.gridstackNode?.id) ?? null;
+    } else if (grid === '2') {
+      this.draggedItem = this.demo8GridElements2.find(el => el.id === event.el.gridstackNode?.id) ?? null;
+    }
+
+    console.log('dragstart', this.draggedItem);
+  }
+
+  protected demo8DroppedCB(event: droppedCB, grid: "1" | "2"): void {
+    console.log('dropped', this.draggedItem);
+
+    // update position for re-creation
+    this.draggedItem!.x = event.newNode.x;
+    this.draggedItem!.y = event.newNode.y;
+    this.draggedItem!.w = event.newNode.w;
+    this.draggedItem!.h = event.newNode.h;
+
+    if (grid === '1') {
+      this.demo8GridElements1 = [...this.demo8GridElements1, this.draggedItem!];
+    } else if (grid === '2') {
+      this.demo8GridElements2 = [...this.demo8GridElements2, this.draggedItem!];
+    }
+
+    this.draggedItem = null
+  }
+
+  protected demo8Removed(event: nodesCB, grid: "1" | "2"): void {
+    console.log('removed', event);
+
+    if (grid === '1') {
+      this.demo8GridElements1 = this.demo8GridElements1.filter(el => el.id !== event.nodes[0].id);
+    } else if (grid === '2') {
+      this.demo8GridElements2 = this.demo8GridElements2.filter(el => el.id !== event.nodes[0].id);
+    }
+  }
+
+  protected demo8Added(event: nodesCB, grid: "1" | "2"): void {
+    console.log('added', event);
   }
 }
